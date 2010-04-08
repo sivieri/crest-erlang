016450ad8d1b11a82d97be51ab333f3657c729e8
Alessandro Sivieri <alex@alex-desktop.(none)>
2010-04-08 13:04:45 +0200

    Working on a test client.

--- include/client.erl
+++ include/client.erl
@@ -11,8 +11,8 @@ get_function() ->
 
 main(_) ->
     inets:start(),
-    % http:set_options([{proxy, {{"localhost", 8080}, []}}]),
-    Res = http:request(post, {"http://localhost:8000/crest/spawn", [], "application/x-crest-serf", term_to_binary(get_function())}, [], []),
+    http:set_options([{proxy, {{"localhost", 8080}, []}}]),
+    Res = http:request(post, {"http://localhost:8000/crest/spawn", [], "application/x-www-form-urlencoded", string:concat("code=", term_to_binary(get_function()))}, [], []),
     case Res of
         {ok, {_, _, Body}} ->
             io:format("Invocazione 1: ~p~n", [http:request("http://localhost:8000/crest/" ++ Body)]),
