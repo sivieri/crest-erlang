f44f716d0969fa80cbe7ba0b4aff9d75a14cb438
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2010-04-16 16:50:28 +0200

    Local tests.

--- src/demo.erl
+++ src/demo.erl
@@ -60,39 +60,39 @@ get_footer() ->
     "</body></html>".
 
 get_function() ->
-    Words = fun(String) ->
+    ClientFunction = fun() ->
+            Words = fun(String) ->
                 {match, Captures} = re:run(String, "\\b\\w+\\b", [global,{capture,first,list}]),
                 [hd(C) || C<-Captures]
             end,
-    ProcessEachLine = fun(ProcessEachLine, IoDevice, Dict) ->
-            case io:get_line(IoDevice, "") of
-                eof -> 
-                    file:close(IoDevice),
-                    Dict;
-                {error, Reason} ->
-                    file:close(IoDevice),
-                    throw(Reason);
-                Data ->
-                    NewDict = lists:foldl(
-                        fun(W, D) -> dict:update(W, fun(C) -> C + 1 end, 1, D) end, 
-                        Dict, 
-                        Words(Data)),
-                    ProcessEachLine(ProcessEachLine, IoDevice, NewDict)
-            end
+            ProcessEachLine = fun(ProcessEachLine, IoDevice, Dict) ->
+                case io:get_line(IoDevice, "") of
+                    eof -> 
+                        file:close(IoDevice),
+                        Dict;
+                    {error, Reason} ->
+                        file:close(IoDevice),
+                        throw(Reason);
+                    Data ->
+                        NewDict = lists:foldl(
+                            fun(W, D) -> dict:update(W, fun(C) -> C + 1 end, 1, D) end, 
+                            Dict, 
+                            Words(Data)),
+                        ProcessEachLine(ProcessEachLine, IoDevice, NewDict)
+                end
             end,
-    GetWordCounts = fun(Filename) ->
-            case file:open(Filename, read) of
-                {ok, IoDevice} ->
-                    Dict = ProcessEachLine(ProcessEachLine, IoDevice, dict:new()),
-                    Dict
-            end
+            GetWordCounts = fun(Filename) ->
+                case file:open(Filename, read) of
+                    {ok, IoDevice} ->
+                        Dict = ProcessEachLine(ProcessEachLine, IoDevice, dict:new()),
+                        Dict
+                end
             end,
-    ClientFunction = fun() ->
             receive
                 {Pid, [{"filename", Filename}, {"limit", Num}]} ->
                     {Limit, _} = string:to_integer(Num),
                     Dict = GetWordCounts(Filename),
-                    HtmlList = dict:fold(fun(Word, Count, AccIn) -> [lists:flatten(io_lib:format("<tr><td>~s</td><td>~s</td></tr>", [Word, Count]))|AccIn] end, [], Dict),
+                    HtmlList = dict:fold(fun(Word, Count, AccIn) -> [lists:flatten(io_lib:format("<tr><td>~s</td><td>~p</td></tr>", [Word, Count]))|AccIn] end, [], Dict),
                     Result = lists:foldl(fun(Element, AccIn) -> AccIn ++ Element end, "<table>", HtmlList),
                     Pid ! {self(), {"text/html", Result ++ "</table>"}};
                 {Pid, Other} ->
