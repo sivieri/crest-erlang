d0fa4a92ab6f0b4cfa5e17d467486a302bbe085d
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2010-11-02 18:26:02 +0100

    New JSON sample.

--- priv/www/manager.js
+++ priv/www/manager.js
@@ -1,17 +1,14 @@
 // jQuery things
-$(document).ready(function()
-{
-	$("#refreshform").submit(function(){
-		$.ajax({
-			url:$("#form2div form").attr("action"),
-			type:"GET",
-			timeout:6000,
-			success: function(data) {
-				
-			},
-			error: function(data, error) {
-    			alert("Error: " + error);
-    		}
-		});
-	});
+$(document).ready(function(){
+	$("#processestable").dataTable({
+		sAjaxSource:"manager",
+		fnServerData:function(sSource, aoData, fnCallback){
+			$.ajax( {
+				type:"GET",
+				url:sSource,
+				timeout:6000,
+				success:fnCallback
+			});
+		}
+	} );
 });
