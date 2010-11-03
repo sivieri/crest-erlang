9c51bed7419c2b498909e27cdc4d617f080e7586
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2010-11-03 12:24:34 +0100

    Manager AJAX call immediately on page load.

--- priv/www/manager.js
+++ priv/www/manager.js
@@ -1,20 +1,26 @@
 // jQuery things
 $(document).ready(function(){
+	createTable();
 	$("#refreshform").submit(function(){
-		$.ajax({
-			url:"manager",
-			type:"GET",
-			timeout:6000,
-			success: function(data) {
-				$("#processestable").dataTable({
-					aaData:data.aaData,
-					bProcessing:true,
-					bDestroy:true
-				});
-			},
-			error: function(data, error) {
-    			alert("Error: " + error);
-    		}
-		});
+		createTable();
 	});
 });
+
+function createTable()
+{
+	$.ajax({
+		url:"manager",
+		type:"GET",
+		timeout:6000,
+		success: function(data) {
+			$("#processestable").dataTable({
+				aaData:data.aaData,
+				bProcessing:true,
+				bDestroy:true
+			});
+		},
+		error: function(data, error) {
+			alert("Error: " + error);
+		}
+	});
+}
