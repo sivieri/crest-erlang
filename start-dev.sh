c5b15d4c585dc0da5b670ddd5aefab8898c8e078
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2011-01-27 10:24:31 +0100

    Updated dev bash script.

--- start-dev.sh
+++ start-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl +P 1000000 -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s crest
+exec erl +P 1000000 -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -sname crest_dev -s crest -s reloader
